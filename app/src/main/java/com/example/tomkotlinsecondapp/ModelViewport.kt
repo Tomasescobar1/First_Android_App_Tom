c453ca7df11e26b9299a053310180a648088702f
Tom <thomasatlascorporation@gmail.com>
2026-01-07 19:19:39 -0500

    Continuig with the box for the 3D Scene in the GuitarViewport component.

--- app/src/main/java/com/example/tomkotlinsecondapp/ModelViewport.kt
+++ app/src/main/java/com/example/tomkotlinsecondapp/ModelViewport.kt
@@ -3,22 +3,31 @@ package com.example.tomkotlinsecondapp
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 //import androidx.compose.material3
 
 @Composable
 fun GuitarViewPort()
 {
-    Box(modifier = Modifier.border(4.dp, Color.Black, RoundedCornerShape(24.dp)).height(300.dp).width(200.dp))
+    Box(modifier = Modifier.border(4.dp, Color.Black, RoundedCornerShape(24.dp))
+        .height(300.dp).width(200.dp), contentAlignment = Alignment.Center)
     {
         Text(
-            text = "This is going to be the guitar viewport"
+            text = "This is going to be the guitar viewport.",
+            fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold, fontSize = 20.sp,
+            modifier = Modifier.padding(15.dp)
         )
     }
 }
